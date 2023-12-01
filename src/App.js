5cefa9121e1c846616d984fc0551202966f8a26a
Tamim <dev7amim@yahoo.com>
2023-12-01 16:16:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url = 'https://www.falstad.com/circuit/circuitjs.html';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <iframe
+        style={{ outline: 'none', border: 'none', width: '100%', height: '100vh' }}
+        src={url}
+        title="No content"
+      ></iframe>
     </div>
   );
 }
